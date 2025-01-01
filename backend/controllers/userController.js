955d4c397796b933030bd8390e192cca309f1db7
imsurajoffcial <suraj030525.com>
2025-01-01 17:10:25 +0530

    commit1

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
